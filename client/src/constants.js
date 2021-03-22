03616645c0e046625bf84da0674660f4bdb271fc
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-22 18:25:07 -0300

    Criando constants.js para substituir palavras que estavam em strings e substituir no terminal controller

---
+++ client/src/constants.js
@@ -0,0 +1,11 @@
+export const constants = {
+  events: {
+    app: {
+      MESSAGE_RECEIVED: "message:received",
+      MESSAGE_SENT: "message:sent",
+      ACTIVITYLOG_UPDATED: "activityLog:updated",
+      STATUS_UPDATED: 'status:updated'
+    },
+    socket: {},
+  },
+};

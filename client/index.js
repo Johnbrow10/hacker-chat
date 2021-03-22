14466ac2021e32273f92fec93707309424d48300
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-22 14:51:28 -0300

    Criando index para inicilizar o projeto com componentEmitter e a biblioteca Events

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import Events from 'events';
+import terminalController from "./src/terminalController.js";
+
+const componentEmitter = new Events();
+
+const controller = new terminalController();
+await controller.initializeTable(componentEmitter);

b00eaec7bdba786483a24bb8d262117992398d67
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-23 15:35:07 -0300

    Testando os comandos para rodar os host de cada usuario

--- client/index.js
+++ client/index.js
@@ -1,7 +1,16 @@
+/*
+ */
 import Events from "events";
+import CliConfig from "./src/cliConfig.js";
 import terminalController from "./src/terminalController.js";
+
+const [nodePath, filePath, ...commands] = process.argv;
+
+const config = CliConfig.parseArguments(commands);
+
+console.log("config", config);
 
 const componentEmitter = new Events();
 
-const controller = new terminalController();
-await controller.initializeTable(componentEmitter);
+// const controller = new terminalController();
+// await controller.initializeTable(componentEmitter);

883b485440de7fa3adf297b003fbaa7b773a8c14
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-23 18:50:04 -0300

    e no index do client chamando o SocketClient e inicializando ccom as configs para mandar e comparar com os dados do server,

--- client/index.js
+++ client/index.js
@@ -2,15 +2,16 @@
  */
 import Events from "events";
 import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
 import terminalController from "./src/terminalController.js";
 
 const [nodePath, filePath, ...commands] = process.argv;
-
 const config = CliConfig.parseArguments(commands);
 
-console.log("config", config);
-
 const componentEmitter = new Events();
+const socketClient = new SocketClient(config);
+
+await socketClient.initialize();
 
 // const controller = new terminalController();
 // await controller.initializeTable(componentEmitter);

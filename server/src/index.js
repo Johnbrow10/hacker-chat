37fe77041aa157a674efbdb2625a35bbd4d582e3
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-23 17:31:48 -0300

    Index criado, com a funcao testServer por enquanto com options de port e host headers com setInterval e eventos que foram capturado com connections e o upgrade sendo para fixar o websocket

---
+++ server/src/index.js
@@ -0,0 +1,47 @@
+import SocketServer from "./socket.js";
+import Event from "events";
+import { constants } from "./constants.js";
+
+const eventEmitter = new Event();
+
+async function testServer() {
+  const options = {
+    port: 9898,
+    host: "localhost",
+    headers: {
+      Connection: "Upgrade",
+      Upgrade: "websocket",
+    },
+  };
+
+  const http = await import("http");
+  const req = http.request(options);
+  req.end();
+
+  req.on("upgrade", (res, socket) => {
+    socket.on("data", (data) => {
+      console.log("client received", data.toString());
+    });
+
+    setInterval(() => {
+      socket.write("Hello!");
+    }, 500);
+  });
+}
+
+const port = process.env.PORT || 9898;
+const socketServer = new SocketServer({ port });
+const server = await socketServer.initialize(eventEmitter);
+
+console.log("socket sever is running at", server.address().port);
+
+eventEmitter.on(constants.event.NEW_USER_CONNECTED, (socket) => {
+  console.log("new Connection!!", socket.id);
+
+  socket.on("data", (data) => {
+    console.log("server received", data.toString());
+    socket.write("World!");
+  });
+});
+
+await testServer();

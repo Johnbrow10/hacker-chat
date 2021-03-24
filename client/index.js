a108a815da186ec9d87b09cffc2dd8a5e06f5cab
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-24 17:18:05 -0300

    Importando o eventManager com os dados de id da sala e o nome do usuario de quando ele entra na sala

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@
  */
 import Events from "events";
 import CliConfig from "./src/cliConfig.js";
+import EventManager from "./src/eventManager.js";
 import SocketClient from "./src/socket.js";
 import terminalController from "./src/terminalController.js";
 
@@ -12,6 +13,14 @@ const componentEmitter = new Events();
 const socketClient = new SocketClient(config);
 
 await socketClient.initialize();
+const eventManager = new EventManager({ componentEmitter, socketClient });
 
-// const controller = new terminalController();
-// await controller.initializeTable(componentEmitter);
+const data = {
+  roomId: config.room,
+  userName: config.username,
+};
+
+eventManager.joinRoomAndWaitForMessages(data);
+
+const controller = new terminalController();
+await controller.initializeTable(componentEmitter);

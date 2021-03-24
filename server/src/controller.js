93a63b71f9a416a08c9edc289b46cfb1d22b5705
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-24 17:22:40 -0300

    funcao para entrar na sala e atualizar globalmente os usuarios que estao na sala, nessa funcao ira retornar apenas os usuarios ativos, #jloinUserRoom ira usar o id da sala e o usuario para setar se estar organizado com os respectivos que ja estao dentro da sala, ja #onSocketData retorna um try cath para verificar se as mensagens estao retornando no formato correto.

--- server/src/controller.js
+++ server/src/controller.js
@@ -1,5 +1,8 @@
+import { constants } from "./constants.js";
+
 export default class Controller {
   #users = new Map();
+  #rooms = new Map();
   constructor({ socketServer }) {
     this.socketServer = socketServer;
   }
@@ -11,19 +14,52 @@ export default class Controller {
     this.#updateGlobalUserData(id, userData);
 
     socket.on("data", this.#onSocketData(id));
-     socket.on("error", this.#onSocketClosed(id));
-     socket.on("end", this.#onSocketClosed(id) );
+    socket.on("error", this.#onSocketClosed(id));
+    socket.on("end", this.#onSocketClosed(id));
+  }
+  async joinRoom(socketId, data) {
+    const userData = data;
+    console.log(`${userData.userName} joined! ${socketId}`);
+    const { roomId } = userData;
+    const user = this.#updateGlobalUserData(socketId, userData);
+    const users = this.#joinUserRoom(roomId, user);
+
+    // retornando apenas os usuarios ativos
+    const currentUsers = Array.from(users.values()).map(({ id, userName }) => ({
+      userName,
+      id,
+    }));
+
+    /* Atualiza o usuario que conectou sobre quais usuarios ja estao conectados na mesma sala!! */
+    this.socketServer.sendMessage(
+      user.socket,
+      constants.event.UPDATE_USERS,
+      currentUsers
+    );
+  }
+
+  #joinUserRoom(roomId, user) {
+    const usersOnRoom = this.#rooms.get(roomId) ?? new Map();
+    usersOnRoom.set(user.id, user);
+    this.#rooms.set(roomId, usersOnRoom);
+
+    return usersOnRoom;
   }
 
   #onSocketData(id) {
     return (data) => {
-      console.log("onSocketData", data.toString());
+      try {
+        const { event, message } = JSON.parse(data);
+        this[event](id, message);
+      } catch (error) {
+        console.error("Wrong event format", data.toString());
+      }
     };
   }
-    
+
   #onSocketClosed(id) {
     return (data) => {
-      console.log("onSocketClosed", data.toString());
+      console.log("onSocketClosed", id);
     };
   }
 

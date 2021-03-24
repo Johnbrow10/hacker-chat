82387869db65c8b9bc0b8ff3d46e2ced332dac00
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-24 19:18:53 -0300

    Funcoes para atualizar o activityLog na tela de qunado o segundo usuario entra na sala assim o primeiro usuario recebe esse aviso no activityLog.

--- client/src/eventManager.js
+++ client/src/eventManager.js
@@ -25,6 +25,21 @@ export default class EventManager {
     this.#updateUsersComponent();
   }
 
+  // chama a constant com o mesmo nome, entao seta todos os usuarios e suas mensagens
+  newUserConnected(message) {
+    const user = message;
+    this.#allUsers.set(user.id, user.userName);
+      this.#updateUsersComponent();
+      this.#updateActivityLogComponent(`${user.userName}`);
+  }
+
+  #updateActivityLogComponent(message) {
+    this.componentEmitter.emit(
+      constants.events.app.ACTIVITYLOG_UPDATED,
+      message
+    );
+  }
+
   #updateUsersComponent() {
     this.componentEmitter.emit(
       constants.events.app.STATUS_UPDATED,

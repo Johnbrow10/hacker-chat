1f4ef5c43af7dadbe17c3df9e15ca233ffb01864
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-24 18:12:00 -0300

    attachEvents tem #serverconnection captura o data e faz um try catch, entao o data tem os metodos toString para retornar uma string, depois um split "\n" para quebrar a string e tirar dela sempre que aparecer um /n e depois um map transformando novamente em um JSON, logo depois outro map com event e message como parametros e setando eles pra emitir no evento

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,8 @@
+import Event from "events";
+
 export default class SocketClient {
   #serverConnection = {};
+  #serverListner = new Event();
   constructor({ host, port, protocol }) {
     this.port = port;
     this.host = host;
@@ -8,6 +11,23 @@ export default class SocketClient {
 
   sendMessage(event, message) {
     this.#serverConnection.write(JSON.stringify({ event, message }));
+  }
+
+  attachEvents() {
+    this.#serverConnection.on("data", (data) => {
+      try {
+        data
+          .toString()
+          .split("\n")
+          .filter((line) => !!line)
+          .map(JSON.parse)
+          .map(({ event, message }) => {
+            this.#serverListner.emit(event, message);
+          });
+      } catch (error) {
+        console.log("invalid!", data.toString(), error);
+      }
+    });
   }
 
   async createConnection() {
